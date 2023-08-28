38bb52f2399deff2cbe8bfaed1dffa488c044e7c
leemimi <mijeong1015@naver.com>
2023-08-28 22:17:14 +0900

    HelloWorld Bean추가

--- src/main/java/com/example/restfulwebservice/HelloWorldController.java
+++ src/main/java/com/example/restfulwebservice/HelloWorldController.java
@@ -3,7 +3,7 @@ package com.example.restfulwebservice;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController //Json형태로 반환됨!
 public class HelloWorldController {
 
     //GET
@@ -11,6 +11,11 @@ public class HelloWorldController {
     @GetMapping("/hello-world")
     public String helloWorld() {
         return "Hello World";
+    }
+
+    @GetMapping("/hello-world-bean")
+    public HelloWorldBean helloWorldBean(){
+        return new HelloWorldBean("Hello world");
     }
 
 }
